322b34b5f42ef4c3d1123e991162b8ec0d66e99e
Mesakh Lolo <mesakhlolo12@gmail.com>
2024-03-15 16:49:23 +0700

    dart loops

---
+++ loops.dart
@@ -0,0 +1,19 @@
+void main() {
+  // For loop
+  var num = 5;
+  for (var i = num; i >= 1; i--) {
+    print(i);
+  }
+
+  // For in loop
+  var names = ["Lolo", "Windah", "Kurniawan"];
+  for (var name in names) {
+    print(name);
+  }
+
+  // While loop
+  while (num >= 1) {
+    print(num);
+    num--;
+  }
+}
