26d261a86b90dc293366492564da09282720d34b
Mesakh Lolo <mesakhlolo12@gmail.com>
2024-03-15 18:31:12 +0700

    dart getting user input

---
+++ input.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  // User input
+  print("Enter your name:");
+
+  // allow the user to enter their name
+  // var name = stdin.readLineSync();
+
+  String? name = stdin.readLineSync();
+
+  print("Halo bang $name");
+}
